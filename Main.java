4fca4545fee41c6288796fd95d0b2134c878e441
khrid0328 <khrid0328@wrdsb.ca>
2020-09-14 15:00:36 +0000

    QUESTION A1Q2

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * This program asks for two numbers and then calculates the quotient of that number and the remainder
+ * @author Dafna Khripun
  */
 public class Main {
 
@@ -9,7 +11,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for two integers on seperate lines to divide
+    System.out.println("Please enter in two integers, on seperate lines, to divide.");
+
+    // declare and initialize a variable for integer1
+    int integer1 = input.nextInt();
+
+    // declare and initialize a variable for integer 2
+    int integer2 = input.nextInt();
+
+    // declare amd calculate the quotient of that number 
+    int quotient = integer1 / integer2;
+
+    //declare amd calculate the remander of that numbers
+    int remainder = integer1 % integer2;
+
+    // tell the user the quotient of that number and the remainder
+    System.out.println(integer1 + "/" + integer2 + " is "+ quotient + " with a remainder of " + remainder + ".");
+
+
   }
 }
